1e06d58d98293d57c63d83ecd0a83190cf3d7074
Bernhard Klingenberg <bklingen@williams.edu>
2022-01-26 13:10:05 -0500

    Created initial dashboard ui.r and server.r files

--- ShinyDashboard/server.R
+++ ShinyDashboard/server.R
@@ -1,26 +1,38 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(readr)
+library(dplyr)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output, session) {
+    
+    df <- reactiveFileReader(
+        intervalMillis = 20000,
+        session = session,
+        filePath = 'https://raw.githubusercontent.com/bklingen/DataViz2022/main/Data/NCAlumn.csv',
+        readFunc = read_csv
+    )
+    
+    output$nCodes <- renderValueBox({
+        
+        if(input$Division=="all") {
+            uniqueCats <- df() %>% select(Industry) %>%
+                distinct() %>%
+                count()
+        } else {
+            uniqueCats <- df() %>% select(DIVISION_1, Industry) %>%
+                filter(DIVISION_1 == input$Division) %>%
+                select(Industry) %>%
+                distinct() %>%
+                count()
+        }
+        
+        valueBox(
+            value = uniqueCats,
+            subtitle = paste("Number of Industry Codes in ", 
+                             ifelse(input$Division=="all", 
+                                    "all Divisions", 
+                                    input$Division))
+        )
     })
-
+    
 })
